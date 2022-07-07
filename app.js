877e088eeb660a05910d2f3c65b1acd3b1d7928f
Anokh <anokhps1998@gmail.com>
2022-07-07 10:12:31 +0530

    another change made

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-function playRound(playerSelection,computerSelection){
-
-
+function computerPlay() {
+    let array = ['rock', 'paper', 'scissor'];
+    let random = array[Math.floor(Math.random() * array.length)];
+    return random;
 }
 
-
-
-const playerSelection=prompt("Enter your take on this game!!").toLowerCase;
-const computerSelection=computerPlay();
-
-console.log(playRound(playerSelection,computerSelection));
-
-
-
-function computerPlay(){
-    let array=['rock','paper','scissor'];
-   let random= array[ Math.floor(Math.random()*array.length)]
-   console.log(random);
-}
-
-function game(){
-
-    for(let i=0; i<1; i++){
-        playRound(playerSelection,computerSelection);
+function yourPlay() {
+    let userInput = prompt('whats your take on this game,rock,paper or scissor').toLowerCase();
+    if (userInput !== 'rock' && userInput !== 'paper' && userInput !== 'scissor') {
+        alert('Wrong take.Please enter rock or paper or scissor!!');
+    } else {
+        return userInput;
     }
 }
-game();
+
+function playRound(playerSelection, computerSelection) {
+
+    let winner=''
+    if (playerSelection === computerSelection) {
+        console.log('Match tied');
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissor') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissor' && computerSelection === 'paper')) {
+            winner='you'
+        console.log(`${winner} win the game`);
+
+    } else {
+        winner='computer'
+        console.log(`${winner} win the game`);
+
+    }
+}
+
+
+
+function game() {
+    let yourScore = 0;
+    let compScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+        
+    }
+}
