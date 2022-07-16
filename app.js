070af9c0debc606d41d7761b2dfb8f5bd2330325
Anokh <anokhps1998@gmail.com>
2022-07-16 17:00:47 +0530

    finally done

--- app.js
+++ app.js
@@ -6,39 +6,55 @@ function computerPlay() {
 
 function yourPlay() {
     let userInput = prompt('whats your take on this game,rock,paper or scissor').toLowerCase();
-    if (userInput !== 'rock' && userInput !== 'paper' && userInput !== 'scissor') {
-        alert('Wrong take.Please enter rock or paper or scissor!!');
-    } else {
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissor') {
         return userInput;
+    } else {
+        alert('Wrong take, enter rock, paper or scissor')
     }
 }
 
+let winner;
 function playRound(playerSelection, computerSelection) {
 
-    let winner=''
-    if (playerSelection === computerSelection) {
-        console.log('Match tied');
+    winner = ''
+    if ((playerSelection === '' || playerSelection === null) && (computerSelection === 'rock' ||
+        computerSelection === 'paper' ||
+        computerSelection === 'scissor')) {
+        return null;
+    } else if (playerSelection === computerSelection) {
+        return 'Match Tied';
+
     } else if ((playerSelection === 'rock' && computerSelection === 'scissor') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissor' && computerSelection === 'paper')) {
-            winner='you'
-        console.log(`${winner} win the game`);
-
-    } else {
-        winner='computer'
-        console.log(`${winner} win the game`);
-
+        winner = 'you';
+        console.log(winner)
+        return `${winner} won the game`;
     }
+    else {
+        winner = 'computer';
+        console.log(winner)
+        return `${winner} won the game`;
+    }
+
 }
-
-
 
 function game() {
     let yourScore = 0;
     let compScore = 0;
 
     for (let i = 0; i < 5; i++) {
-
-        
+        playRound(yourPlay(), computerPlay());
+        if (winner === 'you') {
+            ++yourScore;
+            console.log(yourScore);
+        } else if (winner === 'computer') {
+            ++compScore;
+            console.log(compScore);
+        } else if (winner === undefined) {
+            continue;
+        }
     }
 }
+
+game();
