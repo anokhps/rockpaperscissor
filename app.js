88d2cc77368dbf9ec787f5dc0925de276175512c
Anokh <anokhps1998@gmail.com>
2022-09-06 12:29:10 +0530

    Works have been done.

--- app.js
+++ app.js
@@ -1,62 +1,95 @@
+const playerScreen = document.querySelector('.player');
+const computerScreen = document.querySelector('.computer');
+const resultScreen = document.querySelector('.score')
+const buttons = document.querySelectorAll('button');
+const playerScore = document.querySelector('.p-score');
+const computerScore = document.querySelector('.c-score');
+const finalResult = document.querySelector('.final');
+
+let playerSelection;
+let computerSelection;
+let result;
 function computerPlay() {
-    let array = ['rock', 'paper', 'scissor'];
+    let array = ['👊', '🖐', '✌'];
     let random = array[Math.floor(Math.random() * array.length)];
     return random;
 }
 
-function yourPlay() {
-    let userInput = prompt('whats your take on this game,rock,paper or scissor').toLowerCase();
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissor') {
-        return userInput;
-    } else {
-        alert('Wrong take, enter rock, paper or scissor')
-    }
-}
 
+// for checks who is winner.
 let winner;
 function playRound(playerSelection, computerSelection) {
 
-    winner = ''
-    if ((playerSelection === '' || playerSelection === null) && (computerSelection === 'rock' ||
-        computerSelection === 'paper' ||
-        computerSelection === 'scissor')) {
-        return null;
-    } else if (playerSelection === computerSelection) {
-        return 'Match Tied';
 
-    } else if ((playerSelection === 'rock' && computerSelection === 'scissor') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissor' && computerSelection === 'paper')) {
-        winner = 'you';
-        console.log(winner)
-        return `${winner} won the game`;
+    if ((playerSelection === '' || playerSelection === null)) {
+        return '';
+    } else if (playerSelection === computerSelection) {
+        return resultScreen.textContent = 'Match Tied';
+
+    } else if ((playerSelection === '👊' && computerSelection === '✌') ||
+        (playerSelection === '🖐' && computerSelection === '👊') ||
+        (playerSelection === '✌' && computerSelection === '🖐')) {
+        winner = 'you'
+        resultScreen.textContent = 'you won';
+
+        return `${resultScreen.textContent} won the game`;
     }
     else {
-        winner = 'computer';
-        console.log(winner)
-        return `${winner} won the game`;
+        winner = 'computer'
+        resultScreen.textContent = 'computer won';
+        // console.log(resultScreen.textContent)
+        return `${resultScreen.textContent} won the game`;
     }
 
 }
 
-function game() {
-    let yourScore = 0;
-    let compScore = 0;
+// function game() {
+//     let yourScore = 0;
+//     let compScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        playRound(yourPlay(), computerPlay());
-        if (winner === 'you') {
-            ++yourScore;
-            console.log(yourScore);
-        } else if (winner === 'computer') {
-            ++compScore;
-            console.log(compScore);
-        } else if (winner === undefined) {
+//     for (let i = 0; i < 5; i++) {
+//         playRound(yourPlay(), computerPlay());
+//         if (winner === 'you') {
+//             ++yourScore;
+//             console.log(yourScore);
+//         } else if (winner === 'computer') {
+//             ++compScore;
+//             console.log(compScore);
+//         } else if (winner === undefined) {
 
-            
-            continue;
-        }
-    }
+
+//             continue;
+//         }
+//     }
+// }
+
+// This function is for game ending.
+function gameOver() {
+    buttons.forEach(button => {
+        button.style.display = 'none';
+    })
 }
 
-game();
+let yourScore = 0;
+let compScore = 0;
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    playerSelection = button.textContent;
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    if (winner == 'you') {
+        playerScore.textContent = ++yourScore;
+    } else if (winner == 'computer') {
+        computerScore.textContent = ++compScore;
+    }
+    if (yourScore == 5) {
+        finalResult.textContent = 'you won the game,game over.';
+
+    } else if (compScore == 5) {
+        finalResult.textContent = 'you lost the game,game over.';
+
+    }
+    if (yourScore == 5 || compScore == 5) {
+        gameOver();
+    }
+}));
